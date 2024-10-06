aae05332870dc6ee8fb55adfa60c2dcfd0090d06
seba-solar <seba.solar@duocuc.cl>
2024-10-06 19:17:59 -0300

    Modificacion del index, register, login y creacion de publicacion detalle

--- app.js
+++ app.js
@@ -9,18 +9,18 @@ app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
 
-// // Conexión a la base de datos
-// let conexion = mysql.createConnection({
-//     host: "localhost",
-//     database: "wedo",
-//     user: "root",
-//     password: ""
-// });
+// Conexión a la base de datos
+let conexion = mysql.createConnection({
+    host: "localhost",
+    database: "wedo",
+    user: "root",
+    password: ""
+});
 
-// conexion.connect(function(err) {
-//   if (err) throw err;
-//   console.log("Connected!");
-// });
+conexion.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 //Con este use podemos cargar archivos como javascripts personales para cada interaccion de la pagina
 app.use(express.static(path.join(__dirname, 'public')));
@@ -30,7 +30,7 @@ app.get('/', (req, res) => {
   res.redirect('/index');
 });
 app.get('/index', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index2.html'));
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 app.get('/profile', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'profile.html'));
@@ -43,4 +43,7 @@ app.get('/register', (req, res) => {
 });
 app.get('/publicaciones', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'publicaciones.html'));
+});
+app.get('/publicacion',(req,res) => {
+  res.sendFile(path.join(__dirname,'views','publicacion_detalle.html'));
 });
